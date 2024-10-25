1ad8889a1fd5ed06b8fb792cf570276d3cba928b
Kristina Shvarbukova <shvarbuchok@gmail.com>
2024-10-25 18:11:01 +0300

    change: scroll

--- src/Content/ContentInfiniteScroll.jsx
+++ src/Content/ContentInfiniteScroll.jsx
@@ -1,86 +1,58 @@
-import React, { useEffect, useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { useDispatch, useSelector } from "react-redux";
-import { LIMIT, OFFSET } from '../App';
-import { clearInfo, fetchGetMoreAboutPokemonScroll, fetchGetPokemon, fetchGetPokemonForScroll } from "../redux/slice/listSlice";
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-
-const style = {
-    height: 60,
-    border: "1px solid green",
-    margin: 6,
-    padding: 8
-};
 
 const ContentInfiniteScroll = () => {
-    const dispatch = useDispatch();
-    const { infoScroll } = useSelector(state => state.list);
-    const {dataScroll} = useSelector(state => state.list);
-    const [data, setData] = useState([]);
-    const [offset, setOffset] = useState(5);
-    console.log('dataScroll' , dataScroll);
-    
-    console.log('infoScroll',infoScroll);
-    console.log('data in scroll', data);
+    const [pokemon, setPokemon] = useState([]);
+    const [hasMore, setHasMore] = useState(true);
+    const [offset, setOffset] = useState(0);
+    const limit = 10; // Количество загружаемых элементов
 
+    const fetchPokemon = async () => {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+            const data = await response.json();
 
-    const fetchMoreData = () => {
-        console.log('secound');
-        
-        console.log('offset', offset);
-        console.log('--------------------------------------');
-        setData((data) => [...data, ...infoScroll])
-        
-        dispatch(fetchGetPokemonForScroll({ limit: 5, offset: offset }))
-        setOffset((offset) => offset + 5)
-        
+            // Получаем данные для каждого покемона и добавляем их в массив
+            const pokemonDataPromises = data.results.map(async (p) => {
+                const pokemonResponse = await fetch(p.url);
+                return pokemonResponse.json();
+            });
+
+            const pokemonData = await Promise.all(pokemonDataPromises);
+
+            // Обновляем состояние покемонов с изображениями
+            setPokemon((prevPokemon) => [...prevPokemon, ...pokemonData]);
+            setHasMore(data.results.length > 0); // Устанавливаем флаг, если есть еще покемоны
+            setOffset((prevOffset) => prevOffset + limit); // Увеличиваем смещение для следующего запроса
+        } catch (error) {
+            console.error("Ошибка при загрузке покемонов:", error);
+            setHasMore(false); // В случае ошибки отключаем бесконечную прокрутку
+        }
     };
-    useEffect(()=>{
-        dataScroll.forEach(item => dispatch(fetchGetMoreAboutPokemonScroll(item.url)))
-    }, [dataScroll, dispatch])
-   
+
     useEffect(() => {
-        console.log('first');
-        setData((data) => [...data, ...infoScroll])
-        //dispatch(clearInfo())
-        fetchMoreData()
-    }, [dispatch])
-
-    // const success = () => {
-    //     messageApi.open({
-    //         type: 'success',
-    //         content: '10 покемонов',
-    //     });
-    // };
-
-
-
-
+        fetchPokemon(); // Загружаем покемонов при первом рендере
+    }, []);
 
 
     return (
-        <>
-            <div>
-                <InfiniteScroll
-                    dataLength={data.length}
-                    next={fetchMoreData}
-                    hasMore={true}
-                    loader={<h4>Loading...</h4>}
-                >
-                    {data.map((i, index) => (
-
-                        <div style={style} className="scroll" key={index}>
-                            {i.name}
-                            {console.log('third')}
-                            <img
-                                src={i[i.name].img}
-                                alt={i.name}
-                            />
-                        </div>
-                    ))}
-                </InfiniteScroll>
-            </div>
-        </>
-    )
-}
+        <div>
+            <InfiniteScroll
+                dataLength={pokemon.length}
+                next={fetchPokemon} // Функция загрузки следующей порции данных
+                hasMore={hasMore} // Флаг, указывающий, есть ли еще данные
+                loader={<h4>Загрузка...</h4>} // Сообщение во время загрузки
+                endMessage={<p>Больше покемонов нет</p>} // Сообщение в конце списка
+            >
+                {pokemon.map((p) => (
+                    <div key={p.id} style={{ padding: '10px', borderBottom: '1px solid #ccc', display: 'flex', alignItems: 'center' }}>
+                        <img src={p.sprites.front_default} alt={p.name} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+                        <span>{p.name}</span>
+                    </div>
+                ))}
+            </InfiniteScroll>
+        </div>
+    );
+};
 export default ContentInfiniteScroll;
--- src/redux/slice/listSlice.js
+++ src/redux/slice/listSlice.js
@@ -1,15 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-// const getCount = async () => {
-//     const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1');
-//     //console.log('count', response);
-//     return response.data
-// }
-// const fetchGetCount = createAsyncThunk('count/fetchGetCount', async () => {
-//     const data = await getCount();
-//     console.log('count', data.count);
-//     return data.count
-// })
 
 const getPokemon = async ({limit, offset}) => {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
@@ -21,19 +11,7 @@ const fetchGetPokemon = createAsyncThunk('list/fetchGetPokemon', async (obj) => 
     
     return data;
 })
-const getPokemonScroll = async ({limit, offset}) => {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-    return {results: response.data.results, count:response.data.count};
-}
 
-const fetchGetPokemonForScroll = createAsyncThunk('list/fetchGetPokemonForScroll', async (obj) => {
-    console.log('дошло');
-    
-    const data = await getPokemonScroll(obj);
-    console.log('go', data);
-    
-    return data;
-})
 const getMoreAboutPokemon = async (url) => {
     const response = await axios.get(url);
     //console.log('responseMore', response.data);
@@ -44,18 +22,7 @@ const fetchGetMoreAboutPokemon = createAsyncThunk('info/fetchGetMoreAboutPokemon
     //console.log('dataMore', data);
     return data;
 })
-const getMoreAboutPokemonScroll = async (url) => {
-    const response = await axios.get(url);
-    console.log('responseMore', response.data);
-    return {name: response.data.name, img: response.data.sprites.front_default, id: response.data.id};
-}
-const fetchGetMoreAboutPokemonScroll = createAsyncThunk('info/fetchGetMoreAboutPokemonScroll', async (url) => {
-    console.log('fetchGetMoreAboutPokemonScroll');
-    
-    const data = await getMoreAboutPokemonScroll(url);
-    console.log('dataMore', data);
-    return data;
-})
+
 
 const listSlice = createSlice({
     name: 'list',
@@ -63,8 +30,6 @@ const listSlice = createSlice({
         data: [],
         count: 0,
         info: [],
-        dataScroll: [],
-        infoScroll: [],
         status: null,
         error: null,
     },
@@ -90,14 +55,6 @@ const listSlice = createSlice({
                 state.error = 'failed'
                 state.error = action.payload
             })
-            .addCase(fetchGetPokemonForScroll.fulfilled, (state, action) =>{
-                const {results, count} = action.payload
-                //console.log(count);
-                
-                state.status = 'succeeded'
-                state.dataScroll = results
-                state.count = count
-            })
             .addCase(fetchGetMoreAboutPokemon.pending, (state, action) => {
                 state.status = 'loading'
             })
@@ -106,13 +63,9 @@ const listSlice = createSlice({
                 state.status = 'succeeded'
                 state.info.push({name: name, [name]: {img: img, id: id}});
             })
-            .addCase(fetchGetMoreAboutPokemonScroll.fulfilled, (state, action) => {
-                const {name, img, id} = action.payload
-                state.status = 'succeeded'
-                state.infoScroll.push({name: name, [name]: {img: img, id: id}});
-            })
+            
     }
 })
 export const {clearInfo} = listSlice.actions;
-export { fetchGetPokemon, fetchGetPokemonForScroll, fetchGetMoreAboutPokemon, fetchGetMoreAboutPokemonScroll };
+export { fetchGetPokemon, fetchGetMoreAboutPokemon };
 export default listSlice.reducer;
